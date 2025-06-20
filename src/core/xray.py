691b245f7f26493184829707863d6ee7be4e2969
Stroh Snow <github@stroh.ru>
2025-06-20 03:29:28 +0300

    feat: switch to psutil

--- src/core/xray.py
+++ src/core/xray.py
@@ -1,38 +1,38 @@
 import os
 import subprocess
 
+import psutil
+
 
 class XrayManager:
-    def __init__(self, xray_path: str, config_path: str) -> None:
-        self.xray_path: str = xray_path
+    def __init__(self, executable_path: str, config_path: str) -> None:
+        self.executable_path: str = executable_path
         self.config_path: str = config_path
 
-        self.xray_process: subprocess.Popen | None = None
+        self._process: psutil.Popen | None = None
 
     def is_running(self) -> bool:
-        return self.xray_process is not None and self.xray_process.poll() is None
+        return self._process is not None and self._process.is_running()
 
     def start(self) -> bool:
+        if self.is_running():
+            return True
+
         if not os.path.isfile(self.config_path):
             return False
 
         try:
-            if not self.is_running():
-                self.xray_process = subprocess.Popen(
-                    [self.xray_path, "-c", self.config_path],
-                    creationflags=subprocess.CREATE_NO_WINDOW,
-                )
-                return True
-        except Exception:
-            return False
-        return False
-
-    def stop(self) -> bool:
-        try:
-            if self.is_running():
-                self.xray_process.terminate()
-                self.xray_process.wait(5)
-                self.xray_process = None
+            self._process = psutil.Popen(
+                [self.executable_path, "-c", self.config_path], creationflags=subprocess.CREATE_NO_WINDOW
+            )
             return True
         except Exception:
+            self._process = None
             return False
+
+    def stop(self) -> None:
+        if not self.is_running():
+            return
+
+        self._process.kill()
+        self._process = None
