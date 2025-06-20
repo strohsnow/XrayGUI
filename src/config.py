31330f6d752e6b41fe194c73458b53e299beb322
Stroh Snow <github@stroh.ru>
2025-06-20 03:31:11 +0300

    feat: add tun paths

--- src/config.py
+++ src/config.py
@@ -11,19 +11,21 @@ elif (Path.home() / "AppData" / "Roaming").exists():
 else:
     APPDATA_ROOT = Path.home() / ".config"
 
-BIN_DIR = APP_ROOT / "bin"
 ASSET_DIR = APP_ROOT / "assets"
+BIN_DIR = APP_ROOT / "bin"
 APPDATA_DIR = APPDATA_ROOT / "XrayGUI"
 CONFIG_DIR = APPDATA_DIR / "config"
 LOG_DIR = APPDATA_DIR / "logs"
 
-XRAY_PATH = str(BIN_DIR / "xray.exe")
 ICON_PATH = str(ASSET_DIR / "icon.ico")
-CONFIGS_PATH = str(CONFIG_DIR / "configs.json")
-CONFIG_PATH = str(CONFIG_DIR / "config.json")
-SUB_PATH = str(CONFIG_DIR / "sub.txt")
+SUBSCRIPTION_PATH = str(CONFIG_DIR / "subscription.txt")
+XRAY_PATH = str(BIN_DIR / "xray.exe")
+XRAY_CONFIGS_PATH = str(CONFIG_DIR / "configs.json")
+XRAY_CONFIG_PATH = str(CONFIG_DIR / "config.json")
+TUN_PATH = str(BIN_DIR / "mihomo.exe")
+TUN_CONFIG_PATH = str(CONFIG_DIR / "config.yaml")
 
-USER_AGENT = "Happ/XrayGUI/1.2"
+USER_AGENT = "Happ/XrayGUI/1.3"
 PROXY_IP_ADDR = "127.0.0.1"
 PROXY_PORT = 2080
 SOCKET_NAME = "XrayGUI"
